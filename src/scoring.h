564408cc4a7abb1d836f31ea87d5e010546836f9
Ellen Sun <sunellenyuan@gmail.com>
2025-04-17 14:50:42 -0400

    add temp header files

---
+++ src/scoring.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * takes in its getting from detect about the result
+ * if its success add 1
+ * if fail -1
+ * return intger
+ */
+int update_score(int result);
+
+/**
+ * print out the score in terminal or lcd screen
+ */
+void display();
